8424e9c3d3343b657c59f8f237e3254a1aa31fee
baharibastian <bastian17.98@gmail.com>
2017-06-19 09:48:25 +0700

    lately

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,16 +1,10 @@
 <?php
 
-class Migrate extends CI_Controller
-{
+class migrate extends CI_Controller {
 
-        public function index()
-        {
-                $this->load->library('migration');
-
-                if ($this->migration->current() === FALSE)
-                {
-                        show_error($this->migration->error_string());
-                }
+    public function index() {
+        if(!$this->migration->latest()) {
+             show_error($this->migration->error_string());
         }
-
+    }
 }
